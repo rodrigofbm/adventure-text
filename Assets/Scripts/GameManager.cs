fd84d62b7e1d239c4379442ac0535cac3b691469
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2019-07-29 22:13:28 -0300

    scriptable objects to hand data

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Text txtComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    void Start() {
+        state = startingState;
+        txtComponent.text = state.GetStateStory();
+    }
+
+    void Update() {
+        ChangeState();
+    }
+
+    void ChangeState() {
+        var nextState  = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            state = nextState[0];
+        }else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            state = nextState[1];
+        }
+
+        txtComponent.text = state.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+    [TextArea(14,10)] [SerializeField] string storyTxt;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory() {
+        return storyTxt;
+    }
+
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+}
